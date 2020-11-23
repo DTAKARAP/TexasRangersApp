bbd9177562a883754e17ea40938407cda19f0f7a
FXT8YP9 <fnu_akarapu_divya_tejasvi@homedepot.com>
2020-11-23 15:28:50 -0600

    Initial Commit

---
+++ TexasRangersApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TexasRangersApp
+//
+//  Created by divya akarapu on 11/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
