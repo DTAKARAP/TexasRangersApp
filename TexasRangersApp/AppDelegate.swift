a488fcf8926ea8c9bd3deeac21acf0e614ac31de
Divya <divyatejasvidt@gmail.com>
2020-11-29 19:42:30 -0600

    Code cleanup

--- TexasRangersApp/AppDelegate.swift
+++ TexasRangersApp/AppDelegate.swift
@@ -11,8 +11,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let mainSB = UIStoryboard(name: "Main", bundle: nil)
-        mainSB.instantiateInitialViewController()
         return true
     }
 
